880c23c00391eeb67396d732d50db2a4c65d6f80
Zaccheo Bagnati <zaccheob@gmail.com>
2015-10-14 09:22:48 +0200

    Increased version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 	name='SolrCL',
 	packages = find_packages(),
-        version='0.1.3',
+        version='0.1.4',
         description='SOLR connection library',
         author='Zaccheo Bagnati',
         author_email='zaccheob@gmail.com',
